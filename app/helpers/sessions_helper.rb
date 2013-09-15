0f9f468229fc5393072f2de7240bc8cf00d25557
Stephan <stephan.nawracki@gmx.de>
2013-09-15 18:08:06 +0200

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,9 +22,15 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-
-
-  def sign_out
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
+def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
   end
